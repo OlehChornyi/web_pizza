2e1e502ebdf31dfb434916b030f4089e6cb263ef
oleh <phd.chornyi@gmail.com>
2024-10-29 16:14:18 +0200

    1st: nav and header

---
+++ public/script.js
@@ -0,0 +1,27 @@
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart').onclick = () => {
+    cart.classList.toggle('active');
+    login.classList.remove('active');
+};
+
+let login = document.querySelector('.login-form');
+
+document.querySelector('#login').onclick = () => {
+    login.classList.toggle('active');
+    cart.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
