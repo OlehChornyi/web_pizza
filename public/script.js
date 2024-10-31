4711e97d60dd06003e711e065da59f0ef19b5177
oleh <phd.chornyi@gmail.com>
2024-10-31 09:01:57 +0200

    8th: menu-dialog

--- public/script.js
+++ public/script.js
@@ -42,3 +42,26 @@ var swiper = new Swiper(".menu-slider", {
         clickable: true,
     }
 });
+
+let previewContainer = document.querySelector(".menu-preview-container");
+let previewBox = previewContainer.querySelectorAll('.menu-preview');
+
+document.querySelectorAll('.menu .box').forEach(menu =>{
+    menu.onclick = () =>{
+        previewContainer.style.display = 'flex';
+        let name = menu.getAttribute('data-name');
+        previewBox.forEach(preview =>{
+            let target = preview.getAttribute('data-target');
+            if (name == target){
+                preview.classList.add('active');
+            }
+        });
+    };
+});
+
+previewContainer.querySelector('#close').onclick = () =>{
+    previewContainer.style.display = 'none';
+    previewBox.forEach(close =>{
+        close.classList.remove('active');
+    });
+};
